3f0c5ce4c499463fc9072db32ff816681c1e0f24
sinzng <tlswlgus3547@duksung.ac.kr>
2024-04-09 11:28:32 +0900

    change authentication

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -18,6 +18,58 @@ var connection = new mysql({
     database:process.env.database
 });
 
+function templete_nodata(res) {
+    res.writeHead(200);
+        var templete =`
+        <!DOCTYPE html>
+        <head>
+            <title>Error</title>
+            <meta charset="utf-8" />
+            <link type = "text/css" rel="stylesheet" href="mystyle.css" />
+        </head>
+        <body>
+            <h3>데이터가 존재하지 않습니다.</h3>
+
+        </body>
+        </html>
+    `;
+    res.end(templete);
+    
+}
+
+function templete_result(result, res) {
+    res.writeHead(200);
+        var templete =`
+        <!DOCTYPE html>
+        <head>
+            <title>Error</title>
+            <meta charset="utf-8" />
+            <link type = "text/css" rel="stylesheet" href="mystyle.css" />
+        </head>
+        <body>
+            <table border="1" style="margin:auto;">
+            <thead>
+                <tr><th>User ID</th><th>Password</th></tr>
+            </thead>
+            <tbody>
+            `;
+            for(var i=0; i < result.length; i++) {
+                templete += `
+                    <tr>
+                        <td>${result[i]['userid']}</td>
+                        <td>${result[i]['passwd']}</td>
+                    </tr>
+                `; 
+            }
+            templete += `
+            </tbody>
+            </table>
+        </body>
+        </html>
+    `;
+    res.end(templete);
+}
+
 app.get('/Hello', (req, res) => {
     res.send("Hello World")
 });
@@ -27,7 +79,12 @@ app.get('/select', (req, res) => {
     const result = connection.query("select * from user")
     console.log(result);
     // res.sendStatus(200);
-    res.send(result);
+    // res.send(result);
+    if(result.length==0) {
+        templete_nodata(res);
+    } else {
+        templete_result(result, res);
+    }
 });
 
 // select all rows from st_info table
@@ -35,11 +92,16 @@ app.get('/selectQuery', (req, res) => {
     const id = req.query.id;
     const result = connection.query('select * from user where userid=?', [id]);
     console.log(result);
-    res.send(result);
+    // res.send(result);
+    if(result.length==0) {
+        templete_nodata(res);
+    } else {
+        templete_result(result, res);
+    }
 
-  });
+});
 
-  app.post('/selectQuery', (req, res) => {
+app.post('/selectQuery', (req, res) => {
     const id = req.body.id;
     const result = connection.query('select * from user where userid=?', [id]);
     console.log(result);
@@ -86,9 +148,35 @@ app.post('/login', (req, res) => {
 // register
 app.post('/register', (req, res) => {
     const { id, pw } = req.body;
-    const result = connection.query("insert into user values (?, ?)", [id, pw]);
-    console.log(result);
-    res.redirect('/');
+    if (id == "") {
+        res.redirect("register.html")
+    } else {
+        let result = connection.query('select * from user where userid=?', [id]);
+        if(result.length>0) {
+            res.writeHead(200);
+            var templete =`
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <title>Error</title>
+                    <meta charset="utf-8" />
+                </head>
+                <body>
+                    <div>
+                    <h3 style="margin-left: 30px">Register Failed</h3>
+                    <h4 style="margin-left: 30px">이미 존재하는 아이디 입니다.</h4>
+                    <a href="register.html" style="margin-left:30px">다시 시도하기</a>
+                    </div>
+                </body>
+                </html>
+            `;
+            res.end(templete);
+        } else {
+            const result = connection.query("insert into user values (?, ?)", [id, pw]);
+            console.log(result);
+            res.redirect('/');
+        }
+    }
 });
 
 
