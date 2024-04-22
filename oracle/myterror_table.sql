5a73fdf63e301cd82f0900247e93ecf7efd707b4
sinzng <tlswlgus3547@duksung.ac.kr>
2024-04-22 17:33:20 +0900

    oracle python

---
+++ oracle/myterror_table.sql
@@ -0,0 +1,14 @@
+create table myterror(
+    eventid number,
+    iyear number,
+    iday number,
+    country number,
+    country_next varchar2(255),
+    region nuber,
+    region_txt varchar2(255),
+    prostate varchar2(255),
+    city varcher2(255),
+    latitude number,
+    longitude number
+
+)
