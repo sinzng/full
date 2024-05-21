5985b5b3cb83aa04ec558db8443ca96e8b099428
sinzng <tlswlgus3547@duksung.ac.kr>
2024-05-21 05:43:11 +0000

    docker

--- docker/python/hello.py
+++ docker/python/hello.py
@@ -3,5 +3,5 @@ from bottle import route, run
 @route('/')
 @route('/<name>')
 def index(name="Python"):
-    return 'Hello %s!' % name
-run(host='0.0.0.0', port=8080, theaded=True)
+    return 'Hello %s' % name
+run(host='0.0.0.0', port=3000, threaded=True)
