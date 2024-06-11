7cac19c2c44212a30eb558cb422c499104b4b2c1
sinzng <tlswlgus3547@duksung.ac.kr>
2024-06-11 05:53:59 +0000

    update

---
+++ react/hello_countup/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'; 
+
+class App extends Component {
+  state={
+    
+    count : 0 
+  }; 
+  countup = () => {
+    this.setState({
+      count : this.state.count + 1, 
+    });
+  }; 
+  render() {
+    return(
+      <div className="App">
+       <div>{this.state.count}</div>
+       <button onClick={this.handleChange}>Count Up!!</button>
+    </div>
+    )
+  }
+}
+
+export default App; 
