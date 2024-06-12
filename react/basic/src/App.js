c7948bb3dee224209972fdfd9f731ed1f85cd2b7
sinzng <tlswlgus3547@duksung.ac.kr>
2024-06-12 02:16:56 +0000

    react userlist

--- react/basic/src/App.js
+++ react/basic/src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import Counter from './Counter'; 
+import React, { useRef, useState }from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  return <Counter/>; 
-}
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:'',
+  })
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+  const [users, setUsers] = useState([
+    { id: 1, username: 'developer', email: 'public.developer@gmail.com' },
+    { id: 2, username: 'tester', email: 'public.tester@gmail.com' },
+    { id: 3, username: 'moon', email: 'public.moon@gmail.com' },
+  ]);
 
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id : nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+    setInputs({
+      username:'',
+      email:'',
+    })
+    
+    nextId.current += 1;
+  }
+  return (
+    <>
+    <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+    <UserList users={users}/>
+    </>
+  );
+}
 export default App;
---
+++ react/basic/src/CreateUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreateUser({ username, email, onChange, onCreate }) {
+    return (
+        <div>
+            <input name="username" placeholder="계정명" value={username} onChange={onChange}/>
+            <input name="email" placeholder="이메일" value={email} onChange={onChange}/>
+            <button onClick={onCreate}>등록</button>
+        </div>
+    );
+}
+export default CreateUser; 
