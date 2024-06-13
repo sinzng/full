97f49eb32c5a7c11080bf0e7890d917846dba391
sinzng <tlswlgus3547@duksung.ac.kr>
2024-06-13 09:59:34 +0000

    update

---
+++ ai-1/2_llamaindex/p189_app.py
@@ -0,0 +1,38 @@
+from llama_index import downlad_loader, GPTVectorStoreIndex, ServiceContext, LLMPredictor, LangchainEmbedding
+from llama_index.readers import BeautifulSoupReader
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import HuggingFaceEmbeddings
+from llama_index import Document
+
+loader = BeautifulSoupReader()
+
+documents = loader.load_data(urls=["https://openai.com/blog/planning-for-agi-and-beyong"])
+print("Documents loaded successufully")
+print(f"Loaded documents: {documents}")
+
+Document_objects =[Document(text=doc.text, extra_info=doc.metadata) for doc in documents]
+embed_model = LangchainEmbedding(HuggingFaceEmbeddings(model_name="all-MiniLM-L6-V2"))
+print("Embedding Model initialized successfully")
+
+llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
+print("LLM Predictor initialized successfully")
+
+service_context = ServiceContext.from_defaults(
+    llm_predictor=llm_predictor,
+    embed_model=embed_model
+)
+print("Service Context initialized successfully")
+
+index = GPTVectorStoreIndex.from_documents(
+    Document_objects,
+    service_context=service_context
+)
+print("Index initialized successfully")
+
+query_engine = index.as_query_engine()
+print("Query Engine initialized successfully")
+
+query = "이 웹페이지에서 전하고 싶은 말은 무엇인가요? 한국어로 답해주세요"
+response = query_engine.query(query)
+print(f"query: {query}", end="\n")
+print(f"response: {response}", end="\n")
