b48b389873798d66197189e7cfd33b68a1fa10de
sinzng <tlswlgus3547@duksung.ac.kr>
2024-07-05 01:19:49 +0000

    summary test

--- ai-2/4_summarizer/p207_app.py
+++ ai-2/4_summarizer/p207_app.py
@@ -2,19 +2,25 @@ import requests
 import os
 import textwrap
 
-KAGI_API_KEY = os.environ["KAGI_API_KEY"]
-engines = ["cecil", "agnes", "daphne", "muriel"]
+KAGI_API_KEY = os.environ["KAGI_API_TOKEN"]
+#engines = ["cecil", "agnes", "daphne", "muriel"]
+engines = ["cecil"]
 
 api_url = "https://kagi.com/api/v0/summarize"
-contents_url = "https://www.khan.co.kr/opinion/column/article/202304100300035"
-headers = {"Authorization": "Bot" + KAGI_API_KEY}
+#contents_url = "https://www.khan.co.kr/opinion/column/article/202304100300035"
+contents_url = "https://arxiv.org/pdf/1910.14296v2"
+headers = {"Authorization": "Bot " + KAGI_API_KEY}
 
 for engine in engines:
-    parameters = {"url": contents_url, "engine": engine,"target_language": "ko"}
-    r = requests.get(api_url, headers=headers, params=parameters)
+  parameters = {"url": contents_url, "engine": engine, "target_language": "KO"}
+ 
+  r = requests.get(api_url, headers=headers, params=parameters)
+  
+  print(r.json())
 
-    summary = r.json()['data']['output']
-    shorten_summary = textwrap.shorten(summary,width=150, placeholde=' [...이하 생략...]')
-    
-    print("[요약 엔진]", engine)
-    print("요약 내용(축약) : ", shorten_summary)
+  summary = r.json()['data']['output']
+
+  shorten_summary = textwrap.shorten(summary, width=150, placeholder=' [...이하 생략...]')
+
+  print("[요약 엔진]", engine)
+  print("- 요약 내용(축약) : ", shorten_summary)
--- ai-2/4_summarizer/p210_app.py
+++ ai-2/4_summarizer/p210_app.py
@@ -2,6 +2,7 @@ import requests
 import os
 import textwrap
 
+#text_file_name = "./test.txt"
 text_file_name = "./서연의_이야기.txt"
 
 with open(text_file_name, "r", encoding = "utf-8") as f:
@@ -13,12 +14,17 @@ print(text_data[:158], end ="\n\n")
 KAGI_API_KEY = os.environ["KAGI_API_KEY"]
 
 api_url = "https://kagi.com/api/v0/summarize"
-headers = {"Authorization": "Bot" + KAGI_API_KEY}
-data = {"text":text_data, "target_language":"ko"}
+headers = {"Authorization": "Bot " + KAGI_API_KEY}
+data = {"text":text_data, "target_language":"ko", "engine":"cecil"}
 
-r = requests.post(api_url, headers=headers, data=data)
-print(r)
+#r = requests.post(api_url, headers=headers, data=data)
+#print(r.json())
 
-summary = r.json()['data']['output']
-short_summary = textwrap.shorten(summary, width=150, placeholder=' [...이하 생략...]')
-print("-요약 내용 (축약) : ", short_summary)
+#summary = r.json()['data']['output']
+#short_summary = textwrap.shorten(summary, width=150, placeholder=' [...이하 생략...]')
+#print("-요약 내용 (축약) : ", short_summary)
+
+
+ 
+r = requests.get(api_url, headers=headers, params=data)
+print(r.json())
